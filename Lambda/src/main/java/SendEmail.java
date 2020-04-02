f3a68c5ffc18daee54d3f6d1137c4e854471077a
Sarthak Goel <goel.sar@husky.neu.edu>
2020-04-02 02:09:56 -0400

    Refractoring Code Base & adding ReadME.md

--- Lambda/src/main/java/SendEmail.java
+++ Lambda/src/main/java/SendEmail.java
@@ -17,13 +17,13 @@ import java.util.UUID;
 
 public class SendEmail implements RequestHandler<SNSEvent, Object> {
 
-    private DynamoDB dynamo;
-    private final String TABLE_NAME = "csye6225";
-    private Regions REGION = Regions.US_EAST_1;
+    private DynamoDB dynamoDB;
+    private final String tableName = "csye6225";
+    private Regions region = Regions.US_EAST_1;
     static final String from = System.getenv("emailAddress");
     static final String emailSubject = "Upcoming Due Bills";
     private String body;
-    static String username;
+    static String userName;
 
     //Calculation of Time To Live(TTL)
     int SECONDS_IN_60_MINUTES = 60 * 60;
@@ -41,34 +41,33 @@ public class SendEmail implements RequestHandler<SNSEvent, Object> {
         }
 
         //logging the Events
-        logger.log("domain" + from);
-        logger.log("SNS event=" + request);
-        logger.log("Context=" + context);
-        logger.log("TTL expirationTime=" + expirationTime);
+        logger.log("domain: " + from);
+        logger.log("SNS Published Event: " + request);
+        logger.log("TTL Expiration Time: " + expirationTime);
 
         //Function Execution for extracting the message & user email
         String billMessage = "";
         billMessage = request.getRecords().get(0).getSNS().getMessage();
         String token = UUID.randomUUID().toString();
         String sample = billMessage;
-        username = sample.split(",")[0];
+        userName = sample.split(",")[0];
 
 
         //logging the User Email Address
-        logger.log("Email Address=" + username);
+        logger.log("Email Address of User: " + userName);
         this.initDynamoDbClient();
 
-        Item item = this.dynamo.getTable(TABLE_NAME).getItem("id", username);
+        Item item = this.dynamoDB.getTable(tableName).getItem("id", userName);
 
         if (item == null || (item != null && Long.parseLong(item.get("TTL").toString()) < Instant.now().getEpochSecond())) {
-            this.dynamo.getTable(TABLE_NAME).putItem(new PutItemSpec()
-                    .withItem(new Item().withString("id", username)
+            this.dynamoDB.getTable(tableName).putItem(new PutItemSpec()
+                    .withItem(new Item().withString("id", userName)
                             .withString("Token", token).withLong("TTL", expirationTime)));
 
             StringBuilder stringBuilder = new StringBuilder();
             stringBuilder.append("Hi, \n" + "\nPlease find below the due bill links:");
 
-            logger.log("Before" + billMessage);
+            logger.log("Request Message" + billMessage);
             String[] bills = billMessage.split(",");
 
             for (int i = 1; i < bills.length; i++) {
@@ -87,7 +86,7 @@ public class SendEmail implements RequestHandler<SNSEvent, Object> {
                 Message message = new Message().withSubject(subject).withBody(body);
 
                 SendEmailRequest emailRequest = new SendEmailRequest()
-                        .withDestination(new Destination().withToAddresses(username)).withMessage(message).withSource(from);
+                        .withDestination(new Destination().withToAddresses(userName)).withMessage(message).withSource(from);
 
                 AmazonSimpleEmailService client = AmazonSimpleEmailServiceClientBuilder.standard()
                         .withRegion(Regions.US_EAST_1).build();
@@ -104,8 +103,8 @@ public class SendEmail implements RequestHandler<SNSEvent, Object> {
     //creating a DynamoDB Client
     private void initDynamoDbClient() {
         AmazonDynamoDBClient client = new AmazonDynamoDBClient();
-        client.setRegion(Region.getRegion(REGION));
-        this.dynamo = new DynamoDB(client);
+        client.setRegion(Region.getRegion(region));
+        this.dynamoDB = new DynamoDB(client);
     }
 }
 
